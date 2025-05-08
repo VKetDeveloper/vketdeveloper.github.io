f841cf80294bd8cb9a267398645108732bf1c2e4
RearAlice <120567699+newdays-git@users.noreply.github.com>
2025-05-09 00:54:59 +0900

    fix pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Yusei_Magic} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const YuseiMagic = Yusei_Magic({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "VketREAL有志開発チーム on Github",
