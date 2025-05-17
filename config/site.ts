4e271a8b042f29b3fc7ab72f6e5f04eeced48d4c
RearAlice <120567699+newdays-git@users.noreply.github.com>
2025-05-17 11:40:48 +0900

    fix

--- config/site.ts
+++ config/site.ts
@@ -2,4 +2,8 @@ export const siteConfig = {
   name: "VketREAL 有志開発チーム on Github",
   description:
     "VketREAL 有志開発チームは、VketReal のリアルイベントを支えるために技術開発を行う情熱的なチームです。",
+  navItems: [
+    { name: "ホーム", href: "/" },
+    { name: "GitHub", href: "https://github.com/VKetDeveloper", external: true },
+  ],
 };
