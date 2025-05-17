1b4de6904ce84747e0ea8e34abf01abde1a6d50b
RearAlice <120567699+newdays-git@users.noreply.github.com>
2025-05-17 11:14:43 +0900

    fix

--- .next/static/chunks/pages__app_5771e187._.js
+++ .next/static/chunks/pages__app_5771e187._.js
@@ -1,9 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_next_a166bb8c._.js",
+  "static/chunks/node_modules_next_63387682._.js",
   "static/chunks/node_modules_react-dom_82bb97c6._.js",
-  "static/chunks/node_modules_9e761b3a._.js",
+  "static/chunks/node_modules_4773fbb0._.js",
   "static/chunks/[root-of-the-server]__549a6eff._.js",
   "static/chunks/[root-of-the-server]__992e02bd._.css"
 ],
--- .next/static/chunks/pages_index_5771e187._.js
+++ .next/static/chunks/pages_index_5771e187._.js
@@ -1,11 +1,12 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_@heroui_dom-animation_dist_index_mjs_6e71e539._.js",
-  "static/chunks/node_modules_next_0b19e6de._.js",
+  "static/chunks/node_modules_@heroui_dom-animation_dist_index_mjs_fb404147._.js",
+  "static/chunks/node_modules_next_d439fe35._.js",
   "static/chunks/node_modules_react-dom_82bb97c6._.js",
-  "static/chunks/node_modules_framer-motion_dist_es_7f464765._.js",
-  "static/chunks/node_modules_4616fb74._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_9763eae0._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_7471bf7b._.js",
+  "static/chunks/node_modules_3ef00049._.js",
   "static/chunks/[root-of-the-server]__0780a4ae._.js"
 ],
     source: "entry"
