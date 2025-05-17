96fda6277021878acd6b3fec368d5fd4c9e0a1ef
RearAlice <120567699+newdays-git@users.noreply.github.com>
2025-05-17 11:44:47 +0900

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,30 @@
+// src/components/navbar.tsx
+import React from "react";
 import { siteConfig } from "@/config/site";
 import { NavbarItem } from "@heroui/react";
 import NextLink from "next/link";
 
-{siteConfig.navItems.map((item) => (
-  <NavbarItem key={item.href}>
-    {item.external ? (
-      <a href={item.href} target="_blank" rel="noopener noreferrer">
-        {item.name}
-      </a>
-    ) : (
-      <NextLink href={item.href}>{item.name}</NextLink>
-    )}
-  </NavbarItem>
-))}
+export function Navbar() {
+  return (
+    <nav className="flex space-x-4 p-4 bg-gray-100">
+      {siteConfig.navItems.map((item) => (
+        <NavbarItem key={item.href}>
+          {item.external ? (
+            <a
+              href={item.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              {item.name}
+            </a>
+          ) : (
+            <NextLink href={item.href} legacyBehavior>
+              <a className="text-blue-600 hover:underline">{item.name}</a>
+            </NextLink>
+          )}
+        </NavbarItem>
+      ))}
+    </nav>
+  );
+}
