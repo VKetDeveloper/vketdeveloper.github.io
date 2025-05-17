b17e2d374c97ed19d7b512ec5e9b14eb657a886c
RearAlice <120567699+newdays-git@users.noreply.github.com>
2025-05-17 10:14:36 +0900

    website update

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__f4eb745f._.css"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_page_tsx_72cc0ff7._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_0bc11d70._.js",
-  "static/chunks/_85a21536._.js"
-],
-    source: "dynamic"
-});
